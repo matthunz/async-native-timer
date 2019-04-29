f54b7cdbf61743f5b0a2e056880dbce522484cc7
Matt Hunzinger <matthunz2@gmail.com>
2019-04-28 20:04:32 -0400

    Add docs with example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! # async-native-timer
+//!
+//! ```
+//! #![feature(async_await, await_macro)]
+//!
+//! use std::time::Duration;
+//! use async_native_timer::Delay;
+//!
+//! async move {
+//!     let timer = Delay::new(Duration::from_secs(2));
+//!     await!(timer);
+//! };
+//! ```
+
 use mio::Evented;
 use romio::raw::PollEvented;
 use std::future::Future;
@@ -11,22 +25,27 @@ pub mod raw;
 
 pub use raw::NativeTimer;
 
-
+/// Represents a backend to a native timer
 pub trait RawTimer: Evented {
     fn new_timer() -> Self;
     fn set_timer(&mut self, time: Duration, interval: Option<Duration>);
 }
 
+/// A `Future` that completes after a specified `Duration`.
 pub struct Delay<R: RawTimer> {
     evented: PollEvented<R>,
 }
 
+/// Create a new `Delay` with duration `time`
+///
+/// Uses default native implementation, for a custom `RawTimer` use `from_raw`.
 impl Delay<NativeTimer> {
     pub fn new(time: Duration) -> Self {
         Self::from_raw(NativeTimer::new_timer(), time)
     }
 }
 
+/// Create a new `Delay` from a `RawTimer` implementation with duration `time`.
 impl<R: RawTimer> Delay<R> {
     pub fn from_raw(mut raw: R, time: Duration) -> Self {
         raw.set_timer(time, None);
